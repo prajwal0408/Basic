84a4841138f58d5131498b7567127929295c8b25
Prajwal Narayan <prajwal.narayan@tecnotree.com>
2018-12-21 16:29:30 +0530

    Second commit

---
+++ Add.java
@@ -0,0 +1,15 @@
+public class Add {
+
+//	int a = 10, b = 20;
+	
+	public static void main(String[] args) {
+		
+//		Add x = new Add();
+//		int c = x.a+x.b;
+		
+		int a = 10, b = 20;
+		int c = a+b;
+		System.out.println("Value of c is: "+c);
+	}
+
+}
